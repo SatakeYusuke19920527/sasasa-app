71901b1696572582c5da1ac6588aae35828a2e78
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-02-13 20:29:52 +0900

    2020213

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import { Table } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ComponentA = () => {
   const [count, setCount] = useState(0);
   const [data,setData] = useState([])
-  
 
   useEffect(() => {
     console.log('useEffect が呼び出されました。');
   
-    axios.get('https://jsonplaceholder.typicode.com/todos')
+    axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(res => {
         console.log(res, 'res check')
 setData(res.data)
@@ -23,28 +24,44 @@ setData(res.data)
     <div>
       <div>ComponentA</div>
       <Link to="/componentb" >ComponentBへ移動</Link>
-    <Link to="/componentc" >ComponentCへ移動</Link>
+      <Link to="/componentc" >ComponentCへ移動</Link>
       <div>count : {count}</div>
       <button onClick={() => setCount(count + 1)}>
-       ！ボタンあがる！
+        ！ボタンあがる！
     </button>
-          <button onClick={() => setCount(count - 1)}>
-       ！ボタンさがる！
+      <button onClick={() => setCount(count - 1)}>
+        ！ボタンさがる！
     </button>
     
-          <button onClick={() => setCount(count-count)}>
+      <button onClick={() => setCount(count - count)}>
         ！ボタンりせっと！
     </button>
-      {
-        data.map(d => {
-          return (
-            <div>
-              <div>{d.id}</div>
-              <div>{d.title}</div>
-            </div>
-          )
-        })
-      }
+      
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>userID</th>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map(d => {
+              return (
+                <tr>
+                  <td>{d.userId}</td>
+                  <td>{d.id}</td>
+                  <td>{d.title}</td>
+                <td>{d.body}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </Table>
+
     </div>
   );
 };
